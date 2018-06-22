967e24cf25539ef1ec5eab2cb2392454e4e1d43f
Krishnakanth Yachareni <Krishnakanth_Yachareni@epam.com>
2018-06-22 18:02:39 +0530

    added

--- currency-conversion-service/src/main/java/com/epam/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/epam/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,13 +2,15 @@ package com.epam.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.epam.microservices.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+    }
 }
--- currency-conversion-service/src/main/java/com/epam/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/epam/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -21,11 +21,13 @@ import com.epam.microservices.currencyconversionservice.bean.CurrencyConversionB
  * @FeignClient(name = "currency-exchange-service", url = "localhost:8000")
  */
 
-@FeignClient(name = "currency-exchange-service")
+// @FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
-	public CurrencyConversionBean retrieveExchangeValue(
-			@PathVariable("from") String from, @PathVariable("to") String to);
+    // @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversionBean retrieveExchangeValue(
+            @PathVariable("from") String from, @PathVariable("to") String to);
 
 }
